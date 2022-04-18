9080a3ebea126cf255ba036efd9c2ef331096d44
JoeyShelby <2276852293@qq.com>
2022-04-18 11:31:03 +0800

    complete ReversePolishCalculator

--- src/main/java/myStack/test/TestReversePolishCalculator.java
+++ src/main/java/myStack/test/TestReversePolishCalculator.java
@@ -6,5 +6,6 @@ public class TestReversePolishCalculator {
     public static void main(String[] args) {
         ReversePolishCalculator reversePolishCalculator = new ReversePolishCalculator(" 3 4 + 5 * 6 -");
         System.out.println(reversePolishCalculator.calculate());
+        //
     }
 }
